1fb962ec225cfb5157889283fe211b1336f8828d
DougWSP <107962418+DougWSP@users.noreply.github.com>
2022-06-21 20:01:54 -0300

    Create boot.py

---
+++ import time from umqttsimple import MQTTClient import ubinascii import machine import network import esp esp.osdebug(None) import gc gc.collect()  import json  conf_file = open('conf.json')  conf = json.load(conf_file)  conf_file.close()   # Robot codes  import machine pinNum = 4 #D4  sensor = machine.Pin(pinNum, machine.Pin.IN,  machine.Pin.PULL_UP)  if (sensor.value()):     print( "não há impecilho")      else:     print("há impecilho")  ssid = conf["ssid"] password = conf["password"] mqtt_server = conf["157.230.89.7"] server_port = conf["1883"]  mqtt_user = conf["mqtt"] mqtt_password = conf["oriva_mqtt"]  client_id = ubinascii.hexlify(machine.unique_id()) topic_sub = b'URA01/input' topic_pub = b'URA01/boot.py
@@ -0,0 +1,55 @@
+
+import time
+from umqttsimple import MQTTClient
+import ubinascii
+import machine
+import network
+import esp
+esp.osdebug(None)
+import gc
+gc.collect()
+
+import json 
+conf_file = open('conf.json') 
+conf = json.load(conf_file) 
+conf_file.close() 
+
+# Robot codes 
+import machine
+pinNum = 4 #D4 
+sensor = machine.Pin(pinNum, machine.Pin.IN,  machine.Pin.PULL_UP)
+
+if (sensor.value()):
+    print( "não há impecilho")
+    
+else:
+    print("há impecilho")
+
+ssid = conf["ssid"]
+password = conf["password"]
+mqtt_server = conf["157.230.89.7"]
+server_port = conf["1883"] 
+mqtt_user = conf["mqtt"]
+mqtt_password = conf["oriva_mqtt"]
+
+client_id = ubinascii.hexlify(machine.unique_id())
+topic_sub = b'URA01/input'
+topic_pub = b'URA01/output'
+
+last_message = 0
+message_interval = 1
+counter = 0
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())
+
+
+
